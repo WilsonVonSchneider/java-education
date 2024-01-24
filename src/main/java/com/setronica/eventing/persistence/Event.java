c7ff784e3d55ed8975f79862e5b1a37e2a732fe7
Matej Zagar <matej.zagar@barrage.net>
2024-01-24 14:53:01 +0100

    Add create, show and list features

--- src/main/java/com/setronica/eventing/persistence/Event.java
+++ src/main/java/com/setronica/eventing/persistence/Event.java
@@ -1,14 +1,27 @@
 package com.setronica.eventing.persistence;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
 
 public class Event {
     private int id;
+
+    @NotNull
+    @NotBlank(message = "Title is mandatory field")
     private String title;
+
+    @NotNull
+    @NotBlank(message = "Description is mandatory field")
     private String description;
+
+    @NotNull
+    @NotBlank(message = "Date is mandatory field")
     private LocalDate date;
+
     private List<String> images;
 
     public int getId() {
@@ -49,5 +62,43 @@ public class Event {
 
     public void setImages(List<String> images) {
         this.images = images;
+    }
+
+    /**
+     * Sorts a list of events based on a specified parameter and order.
+     *
+     * @param events The list of events to be sorted.
+     * @param sortBy The parameter by which to sort the events ("title" or "date").
+     * @param sort   The order of sorting ("asc" for ascending, "desc" for descending).
+     *               Note: The function assumes "asc" if the provided value is not "desc".
+     * @return A sorted list of events based on the specified parameter and order.
+     */
+    public static List<Event> sort(List<Event> events, String sortBy, String sort) {
+        Comparator<Event> comparator = switch (sortBy.toLowerCase()) {
+            case "title" -> Comparator.comparing(Event::getTitle);
+            case "date" -> Comparator.comparing(Event::getDate);
+            default -> throw new IllegalArgumentException("Invalid sortBy option: " + sortBy);
+        };
+
+        if ("desc".equalsIgnoreCase(sort)) {
+            comparator = comparator.reversed();
+        }
+
+        events.sort(comparator);
+        return events;
+    }
+
+    /**
+     * Retrieves a list of demo images URLs for testing or demonstration purposes.
+     *
+     * @return A list of demo image URLs.
+     * Note: This function provides a set of demo images that can be used for testing or demonstration.
+     */
+    public static List<String> getDemoImages() {
+        return List.of(
+                "https://www.rollingstone.com/wp-content/uploads/2022/12/TNMRollingStone_FrontLine-3.jpg?w=1581&h=1054&crop=1",
+                "https://theatre.colostate.edu/wp-content/uploads/sites/11/2017/02/apocalyptica_concert_in_warsaw_by_lestreux-d31m8k5.png",
+                "https://thestagesofmn.files.wordpress.com/2022/06/2022mayrichryan202.jpg?w=1024"
+        );
     }
 }
--- src/main/java/com/setronica/eventing/persistence/EventRepository.java
+++ src/main/java/com/setronica/eventing/persistence/EventRepository.java
@@ -3,7 +3,10 @@ package com.setronica.eventing.persistence;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,21 +14,36 @@ import java.util.List;
 public class EventRepository {
 
     private final ObjectMapper objectMapper;
+    private static final String JSON_FILE_PATH = "/static/events.json";
+
 
     public EventRepository(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
     }
 
-    public List<Event> findAll() {
-        try (InputStream inputStream = EventRepository.class.getResourceAsStream("/static/events.json")) {
-            if (inputStream != null) {
-                return objectMapper.readValue(inputStream, EventList.class);
+    public List<Event> getAll() {
+        try (InputStream inputStream = EventRepository.class.getResourceAsStream(JSON_FILE_PATH)) {
+            if (inputStream == null) {
+                return Collections.emptyList();
             }
-            return Collections.emptyList();
+
+            return objectMapper.readValue(inputStream, EventList.class);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         return null;
+    }
+
+    public void create(List<Event> events) {
+        try {
+            String json = objectMapper.writeValueAsString(events);
+            Path filePath = Path.of(getClass().getResource(JSON_FILE_PATH).toURI());
+            Files.writeString(filePath, json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
